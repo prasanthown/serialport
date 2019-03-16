43d12093d3d1616f0c8ed50d36f6e1fd050c500c
prasanth <prasanth1036@gmail.com>
2019-03-16 10:02:15 +0530

    Update serialport.php
    Read data from port

--- serialport.php
+++ serialport.php
@@ -1,4 +1,8 @@
 <?php
-	
-
-?>
+    	 exec('powershell [System.IO.Ports.SerialPort]::getportnames()', $port_list);
+	 print_r($port_list) //list of comport connection
+	 
+	 $port ='COM3' //specify some port to get read value from port
+	 exec('powershell -ExecutionPolicy ByPass -File .\comport.ps1 -port '.$port,$res);
+	 echo $res;
+ ?>
